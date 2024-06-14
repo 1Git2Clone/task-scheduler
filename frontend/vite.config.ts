f755a6276b0d84a3f43ab14cbf54d0efb42167c8
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-06-15 01:22:43 +0300

    Removed unneeded .env and server comments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-// import fs from "fs";
-// import path from "path";
-// import dotenv from "dotenv";
-
-// dotenv.config({ path: path.resolve(__dirname, "../backend/.env") });
 
 export default defineConfig({
   plugins: [react(), reactRefresh()],
@@ -15,16 +10,5 @@ export default defineConfig({
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
     },
-  },
-  server: {
-    // https: {
-    //   key: fs.readFileSync(path.resolve(__dirname, "../backend/cert/key.pem")),
-    //   cert: fs.readFileSync(
-    //     path.resolve(__dirname, "../backend/cert/cert.pem"),
-    //   ),
-    //   passphrase: process.env.SSL_PASSWORD,
-    // },
-    // port: 42210,
-    // strictPort: true,
   },
 });
