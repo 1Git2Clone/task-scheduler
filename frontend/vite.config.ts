9f454df9691c6ab2a5d9405d4069f76ebb6d8b97
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-06-15 13:15:34 +0300

    Added dynamic repo name base url
    THIS DEPLOYMENT FINALLY WORKSSSSS **WOOOOOOOOOOO**!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,11 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
+// This is dynamically processed for the GitHub Pages CI/CD.
+const repoName = process.env.REPO_NAME || "";
+
 export default defineConfig({
-  base: "/task-scheduler/",
+  base: `${repoName}`,
   plugins: [react(), reactRefresh()],
   css: {
     postcss: {
